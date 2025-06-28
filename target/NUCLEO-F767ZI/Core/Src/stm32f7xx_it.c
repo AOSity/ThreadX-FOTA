d8c6721bfc66d57ab718c6c663ce70b738712d2d
Andrii Horbul <andreyhorbggwp@gmail.com>
2025-06-28 15:05:48 +0300

    Change timebase source to TIM6

--- target/NUCLEO-F767ZI/Core/Src/stm32f7xx_it.c
+++ target/NUCLEO-F767ZI/Core/Src/stm32f7xx_it.c
@@ -56,6 +56,8 @@
 
 /* External variables --------------------------------------------------------*/
 extern ETH_HandleTypeDef heth;
+extern TIM_HandleTypeDef htim6;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -185,7 +187,7 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
+
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
@@ -197,6 +199,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles TIM6 global interrupt, DAC1 and DAC2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
 
 /**
   * @brief This function handles Ethernet global interrupt.
--- target/NUCLEO-F767ZI/target.cmake
+++ target/NUCLEO-F767ZI/target.cmake
@@ -37,6 +37,9 @@ set(TARGET_SOURCES
     Core/Src/system_stm32f7xx.c
     Core/Src/eth.c
     Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_eth.c
+    Core/Src/stm32f7xx_hal_timebase_tim.c
+    Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim.c
+    Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim_ex.c
     startup_stm32f767xx.s
 )
 list(TRANSFORM TARGET_SOURCES PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
